282d2e929d2e6b58c795b756e8658142cbbc6b37
marusaez <marui.saez@gmail.com>
2021-11-23 23:02:08 -0300

    commit added Switch label

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {Switch, Route } from "react-router-dom";
 import Tweets from "./Tweets"
 import Login from "./Login"
 import Register from "./Register"
@@ -9,12 +10,12 @@ function Main() {
   
   return (
     <main className="App">
-      <Routes>
+     <Switch>
         <Route exact path="/" component={Home}/>
         <Route exact path="/Tweets" component={Tweets}/>
         <Route exact path="/Login" component={Login}/>
         <Route exact path="/Register" component={Register}/>
-      </Routes>
+    </Switch>
     </main>
   );
 }
